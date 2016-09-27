570f6c48c7edf625125510097540311dce349db1
rustamse <rustamse@gmail.com>
2016-09-27 19:27:21 +0500

    Вынес функцию getMovieAmount, автоматически отформатирвоал весь код ctrl+alt+l

--- store.js
+++ store.js
@@ -1,73 +1,79 @@
 "use strict";
 
 function statement(customer, movies) {
-  let totalAmount = 0;
-  let totalBonusPoints = 0;
-  let result = `Rental Record for ${customer.name}\n`;
-  for (let rental of customer.rentals) {
-    let movie = getMovie(rental);
-    let thisAmount = 0;
+    let totalAmount = 0;
+    let totalBonusPoints = 0;
+    let result = `Rental Record for ${customer.name}\n`;
 
-    // determine amount for each movie
-    switch (movie.code) {
-      case "regular":
-        thisAmount = 2;
-        if (rental.days > 2) {
-          thisAmount += (rental.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = rental.days * 3;
-        break;
-      case "childrens":
-        thisAmount = 1.5;
-        if (rental.days > 3) {
-          thisAmount += (rental.days - 3) * 1.5;
-        }
-        break;
+    function getMovie(rental) {
+        return movies[rental.movieID];
     }
 
-    //add frequent renter points
-    totalBonusPoints++;
-    // add bonus for a two day new release rental
-    if (movie.code === "new" && rental.days > 2) totalBonusPoints++;
+    function getMovieAmount() {
+        let thisAmount = 0;
 
-    //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n`;
-    totalAmount += thisAmount;
-  }
-  // add footer lines
-  result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${totalBonusPoints} frequent renter points\n`;
+        // determine amount for each movie
+        switch (movie.code) {
+            case "regular":
+                thisAmount = 2;
+                if (rental.days > 2) {
+                    thisAmount += (rental.days - 2) * 1.5;
+                }
+                break;
+            case "new":
+                thisAmount = rental.days * 3;
+                break;
+            case "childrens":
+                thisAmount = 1.5;
+                if (rental.days > 3) {
+                    thisAmount += (rental.days - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
 
-  return result;
+    for (let rental of customer.rentals) {
+        let movie = getMovie(rental);
+        var thisAmount = getMovieAmount();
 
-  function getMovie(rental) {
-    return  movies[rental.movieID];
-  }
+        //add frequent renter points
+        totalBonusPoints++;
+        // add bonus for a two day new release rental
+        if (movie.code === "new" && rental.days > 2) totalBonusPoints++;
+
+        //print figures for this rental
+        result += `\t${movie.title}\t${thisAmount}\n`;
+        totalAmount += thisAmount;
+    }
+    // add footer lines
+    result += `Amount owed is ${totalAmount}\n`;
+    result += `You earned ${totalBonusPoints} frequent renter points\n`;
+
+    return result;
 }
 
 let customer = {
-  name: "martin",
-  rentals: [{
-    "movieID": "F001",
-    "days": 3
-  }, {
-    "movieID": "F002",
-    "days": 1
-  }, ]
+    name: "martin",
+    rentals: [{
+        "movieID": "F001",
+        "days": 3
+    }, {
+        "movieID": "F002",
+        "days": 1
+    },]
 };
 
 let movies = {
-  "F001": {
-    "title": "Ran",
-    "code": "regular"
-  },
-  "F002": {
-    "title": "Trois Couleurs: Bleu",
-    "code": "regular"
-  },
-  // etc
+    "F001": {
+        "title": "Ran",
+        "code": "regular"
+    },
+    "F002": {
+        "title": "Trois Couleurs: Bleu",
+        "code": "regular"
+    },
+    // etc
 };
 
 console.log(statement(customer, movies));
